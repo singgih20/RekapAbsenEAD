e910d8c6e03ef0f4a46dbdef1ce4f7b9f5c91049
singgih20 <48742686+singgih20@users.noreply.github.com>
2019-03-30 09:59:05 +0700

    function request added

--- functions.php
+++ functions.php
@@ -15,19 +15,19 @@ function query($query){
 function tambah($data){
 	global $conn;
 	$nama = $data["nama"];
-	$nim = $data["nim"];
-	$kelas = $data["kelas"];
+	$kode = $data["kode"];
+	$shift = $data["shift"];
+	$jam = $data["jam"];
 	$tanggal = $data["tanggal"];
-	$keterangan = $data["keterangan"];
 
-	$query = "INSERT INTO mahasiswa VALUES('', '$nama', '$nim', '$kelas', '$tanggal', '$keterangan')";
+	$query = "INSERT INTO assistant VALUES('', '$nama', '$kode', '$shift', '$jam', '$tanggal')";
 	mysqli_query($conn, $query);
 	return mysqli_affected_rows($conn);
 }
 
 function hapus($id){
 	global $conn;
-	$query = "DELETE FROM mahasiswa WHERE id = $id";
+	$query = "DELETE FROM assistant WHERE id = $id";
 	mysqli_query($conn, $query);
 	return mysqli_affected_rows($conn);
 }
@@ -36,14 +36,60 @@ function ubah($data){
 	global $conn;
 	$id = $data["id"];
 	$nama = $data["nama"];
-	$nim = $data["nim"];
-	$kelas = $data["kelas"];
+	$kode = $data["kode"];
+	$shift = $data["shift"];
+	$jam = $data["jam"];
 	$tanggal = $data["tanggal"];
-	$keterangan = $data["keterangan"];
 
-	$query = "UPDATE mahasiswa SET nama ='$nama', nim = '$nim', kelas = '$kelas', tanggal = '$tanggal', keterangan = '$keterangan' WHERE id = $id";
+	$query = "UPDATE assistant SET nama ='$nama', kode = '$kode', shift = '$shift', jam = '$jam', tanggal = '$tanggal' WHERE id = $id";
 	mysqli_query($conn, $query);
 	return mysqli_affected_rows($conn);
  }
 
+function registrasi($data){
+	global $conn;
+
+	$username = strtolower(stripslashes( $data["username"]));
+	$password = mysqli_real_escape_string($conn, $data["password"]);   //memungkinkan user mengunakan tanda kutip
+	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+	//cek user tersedia
+	$result = mysqli_query($conn, "SELECT username FROM user WHERE username='$username'");
+
+	if(mysqli_fetch_assoc($result) ){
+		echo "<script>
+		alert('username sudah terdaftar');
+		</script>";
+		return false;
+	}
+
+
+	//cek konfirmasi password
+	if($password !== $password2){
+		echo "<script> 
+			alert('konfirmasi password tidak sesuai');
+			</script>";
+			return false;
+	}
+
+	$password = password_hash($password, PASSWORD_DEFAULT);
+
+	mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
+	return mysqli_affected_rows($conn);
+}
+
+function request($data){
+	global $conn;
+	$nama = $data["nama"];
+	$kode = $data["kode"];
+	$shift = $data["shift"];
+	$jam = $data["jam"];
+	$tanggal = $data["tanggal"];
+	$gambar = $data["gambar"];
+	$query = "INSERT INTO request VALUES('', '$nama', '$kode', '$shift', '$jam', '$tanggal', '$gambar' )";
+	mysqli_query($conn, $query);
+	return mysqli_affected_rows($conn);
+}
+
  ?>
+
