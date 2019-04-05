b7f32a250d7b3bc17f23a882bfa8bef7c9302a6f
afina <afinaramadhani09@gmail.com>
2019-04-05 17:37:41 +0700

    tampilan baru

--- registrasi.php
+++ registrasi.php
@@ -3,76 +3,68 @@
 require 'functions.php';
 
 if(isset($_POST["register"]) ){
-	if(registrasi($_POST) > 0 ){
-		echo "<script> alert('user baru berhasil ditambahkan');
-		document.location.href='login.php'
-		</script>";
-	}else{
-		echo "<script> alert('user baru gagal ditambahkan');
-		</script>";
-	}
+  if(registrasi($_POST) > 0 ){
+    echo "<script> alert('user baru berhasil ditambahkan');
+    document.location.href='login.php'
+    </script>";
+  }else{
+    echo "<script> alert('user baru gagal ditambahkan');
+    </script>";
+  }
 }
 
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="halregister.css">
 
-	
-	<title>Halaman Registrasi</title>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Halaman Registrasi</title>
+  </head>
+  <body>
+   <div class="container">
+     <h2 class ="text-center">Halaman Registrasi</h2>
 
+     <form action="" method="post">
+  <div class="form-group">
+    <label for="username">Username</label>
+    <input type="username" name="username" class="form-control" id="username"  placeholder="Username">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="password2">Konfirmasi Password</label>
+    <input type="password" name="password2" class="form-control" id="password2" placeholder="Password">
+  </div>
+  <div class="form-group form-check"></div>
+  <div class="form-group">
+    <label for="nama">Nama</label>
+    <input type="nama" name="nama" class="form-control" id="nama" placeholder="Nama">
+  </div>
 
-	<link rel="stylesheet"  href="bootstrap/css/bootstrap.css">
-	<link rel="stylesheet"  href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet"  href="font-awesome/css/fontawesome.min.css">
-	<link rel="stylesheet"  href="font-awesome/css/fontawesome.css">
+  <div class="form-group">
+    <label for="kode">Kode Asisten</label>
+    <input type="kode" name="kode" class="form-control" id="kode" placeholder="Kode Asisten">
+  </div>
+  <button type="submit" name="register"class="btn btn-primary">Register</button>
+ 
+</form>
+   </div>
 
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
-	<link rel="stylesheet" href="dist/css/admin.css">
-</head>
-<body>
-
-	<h1>Halaman Registrasi</h1>
-
-	<form action="" method="post">
-
-		<ul>
-			<li>
-				<label for="username" > Username: </label>
-				<input type="text"  class="form-control" name="username" id="username" required>
-			</li>
-			<li>
-				<label for="password"> Password </label>
-				<input type="password" class="form-control"  name="password" id="password" required>
-			</li>
-			<li>
-				<label for="password2"> Confirm Password: </label>
-				<input type="password" class="form-control" name="password2" id="password2">
-			</li>
-			<li>
-				<label for="nama">Nama</label>
-				<input type="text"class="form-control"  name="nama" id="nama" required>
-			</li>
-			<li>
-				<label for="kode">Kode Assistant</label>
-				<input type="text"class="form-control"  name="kode" id="kode" required>
-			</li>
-			<li>
-				<button type="submit" class="btn btn-primary" name="register">Register! </button>
-			</li>
-
-		</ul>
-
-	</form>
-	<script src="dist/js/jquery.min.js"></script> 
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="dist/js/admin.js"></script>
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
