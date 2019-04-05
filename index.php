dd0a5a24dc4d2d04a1761c0907da5336ee03c760
singgih20 <48742686+singgih20@users.noreply.github.com>
2019-04-05 17:15:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,20 @@ if(!isset($_SESSION["admin"]) ){
 
 
 require 'functions.php';
-$assistant = query("SELECT * FROM assistant ORDER BY jam ASC");
+$jumlahDataPerhalaman = 10;
+$jumlahData = count(query("SELECT * FROM assistant"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
+$halamanAktif = (isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1 ;
+$awalData = ($jumlahDataPerhalaman * $halamanAktif) - $jumlahDataPerhalaman;
+
+$assistant = query("SELECT * FROM assistant LIMIT $awalData, $jumlahDataPerhalaman");
+
 
 
 if(isset($_POST["cari"]) ){
 	$keyword = $_POST['keyword'];
 	$assistant = query("SELECT * FROM assistant WHERE nama LIKE '%$keyword%' OR kode LIKE '%$keyword%'
-			");
+		");
 }
 
 
@@ -74,8 +81,8 @@ if(isset($_POST["cari"]) ){
 						<a href="#"><i class="fa fa-cube"></i> <span>Data Master</span><i class="fa fa-angle-down pull-right" ></i></a>
 						<ul>
 							<li><a href="tambah.php"><i class="fas fa-plus"></i> Tambah Absen</a></li>
-							<li><a href="request.php"><i class="fal fa-sticky-note"></i> Lihat Daftar Request</a></li>
-							<li><a href=""><i class="fas fa-print"></i> Cetak</a></li>
+							<li><a href="request.php"><i class="far fa-clipboard"></i> Lihat Daftar Request</a></li>
+							<li><a href="cetak.php" target="_blank"><i class="fas fa-print"></i> Cetak</a></li>
 						</ul>
 					</li>
 					
@@ -96,44 +103,57 @@ if(isset($_POST["cari"]) ){
 				<!-- <div class="container">
 					<div class="row">
 						<div class="col-md-12"> -->
-				<table class="table table-hover table-striped table-bordered" border="1" cellpadding="10" cellspacing="0">
+							<table class="table table-hover table-striped table-bordered" border="1" cellpadding="10" cellspacing="0">
 			<!-- 	</div>
 			</div>
 		</div> -->
 
-					<tr>
-						<th>No.</th>
-						<th>Nama</th>
-						<th>Kode Assistant</th>
-						<th>Jam Masuk</th>
-						<th>Tanggal</th>
-						<th class = "aksi">Aksi</th>
-					</tr>
-					<?php $no = 1; ?>
-					<?php foreach($assistant as $mhs): ?>
-						<tr>
-							<td><?= $no; ?></td>
-							<td><?= $mhs["nama"]; ?></td>
-							<td><?= $mhs["kode"]; ?></td>
-							<td><?= $mhs["jam"]; ?></td>
-							<td><?= $mhs["tanggal"];?> </td>
-							<td class="aksi">
-								<a href="edit.php?id= <?= $mhs["id"]; ?>"> Edit Data | | </a>
-								<a href="hapus.php?id= <?= $mhs["id"]; ?>" onclick="return confirm('Apakah anda yakin?')"><i class="fas fa-trash-alt"></i></a>
-							</td>
-						</tr>
-						<?php $no++; ?>
-					<?php endforeach; ?>
+		<tr>
+			<th>No.</th>
+			<th>Nama</th>
+			<th>Kode Assistant</th>
+			<th>Jam Masuk</th>
+			<th>Tanggal</th>
+			<th class = "aksi">Aksi</th>
+		</tr>
+		<?php $no = 1; ?>
+		<?php foreach($assistant as $mhs): ?>
+			<tr>
+				<td><?= $no; ?></td>
+				<td><?= $mhs["nama"]; ?></td>
+				<td><?= $mhs["kode"]; ?></td>
+				<td><?= $mhs["jam"]; ?></td>
+				<td><?= $mhs["tanggal"];?> </td>
+				<td class="aksi">
+					<a href="edit.php?id= <?= $mhs["id"]; ?>"> Edit Data | | </a>
+					<a href="hapus.php?id= <?= $mhs["id"]; ?>" onclick="return confirm('Apakah anda yakin?')"><i class="fas fa-trash-alt"></i></a>
+				</td>
+			</tr>
+			<?php $no++; ?>
+		<?php endforeach; ?>
 
 
-				</table>
-			</div>
-		</section>
+	</table>
+	<ul class="pagination">
+		<?php for($i = 1; $i<= $jumlahHalaman; $i++) : ?>
+			<?php if($i == $halamanAktif): ?>
+				<?php if($halamanAktif>1): ?>
+					<li class="page-item"><a class="page-link" href="?halaman=<?= $halamanAktif -1; ?>">Previous</a></li>
+				<?php endif; ?>
+				<li class="page-item"><a class="page-link" href="?halaman=1">1</a></li>
+				<li class="page-item"><a class="page-link" href="?halaman=2">2</a></li> 
+				<li class="page-item"><a class="page-link" href="?halaman=<?= $halamanAktif +1; ?>">Next</a></li>
+			</ul>
+			<?php else: ?>
+			<?php endif; ?>
+		<?php endfor; ?>
 	</div>
+</section>
+</div>
 
 
-	<script src="dist/js/jquery.min.js"></script> 
-	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<script src="dist/js/admin.js"></script>
+<script src="dist/js/jquery.min.js"></script> 
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="dist/js/admin.js"></script>
 </body>
 </html>
