c70d60c57059870c3a693f93cf7bb9a218fe57b1
singgih20 <48742686+singgih20@users.noreply.github.com>
2019-04-05 15:43:31 +0700

    final commit

--- absen.sql
+++ absen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2019 at 04:02 AM
+-- Generation Time: Apr 05, 2019 at 10:40 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'singgih', '1234'),
+(2, 'aji', '1234'),
+(3, 'admin', '1234');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `assistant`
 --
 
@@ -32,63 +53,29 @@ CREATE TABLE `assistant` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `kode` char(3) NOT NULL,
-  `shift` varchar(50) NOT NULL,
-  `jam` varchar(50) NOT NULL,
-  `tanggal` varchar(50) NOT NULL
+  `jam` time NOT NULL,
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `assistant`
 --
 
-INSERT INTO `assistant` (`id`, `nama`, `kode`, `shift`, `jam`, `tanggal`) VALUES
-(3, 'Zaqlul', 'ZAQ', 'shift1', '15:20:30', '20/10/2019'),
-(4, 'Nur Azizah ', 'CHA', 'shift1', '15:20:30', '20/10/2019'),
-(5, 'Jeremiah', 'JMY', 'shift1', '15:30:38', '20/10/2019');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mahasiswa`
---
-
-CREATE TABLE `mahasiswa` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `nim` char(10) NOT NULL,
-  `kelas` varchar(10) NOT NULL,
-  `tanggal` varchar(20) NOT NULL,
-  `keterangan` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `mahasiswa`
---
-
-INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `kelas`, `tanggal`, `keterangan`) VALUES
-(1, 'Singgih Aji Sasongko', '1202172192', 'SI4105', '10/20/2019', '  Izin'),
-(2, 'Muhammad Zaqlul', '1202173142', 'SI4101', '10/20/2019', 'Sakit'),
-(3, 'Erado Fata Sanjaya', '1202173424', 'SI4102', '10/20/2019', 'Hadir'),
-(5, 'Ahmad Sambang Setya Tama', '1202174243', 'SI4105', '20/10/2019', 'Hadir'),
-(6, 'Radita ', '1202173333', 'SI4103', '20/10/2019', 'Hadir'),
-(7, 'Muhammad Arrasyidi', '1202172321', 'SI4105', '20/10/2019', 'Hadir'),
-(8, 'Muhammad Zaki S', '1202173246', 'SI4105', '20/10/2019', 'Hadir'),
-(9, 'Izha Mahendra', '1202174424', 'SI4105', '20/10/2019', 'Hadir'),
-(10, 'Zaqlul', 'ZAQ', 'shift1', '15:30:38', '2019:Mar:2');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mahasiswa1`
---
-
-CREATE TABLE `mahasiswa1` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `kode` char(3) NOT NULL,
-  `shift` varchar(10) NOT NULL,
-  `jam` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `assistant` (`id`, `nama`, `kode`, `jam`, `tanggal`) VALUES
+(14, 'Lina', 'LNA', '14:00:00', '2019-10-20'),
+(15, 'Muhammad Arrasyid Rakhmadzan', 'ALB', '23:12:00', '2019-12-20'),
+(16, 'Muhammad Rafly', 'MRF', '23:10:00', '2019-10-20'),
+(17, 'Muhammad Zaqlul', 'ZAQ', '11:20:00', '2019-10-20'),
+(18, 'Habib Arrachman', 'HBB', '14:02:00', '2019-10-20'),
+(19, 'Lala Arif', 'LAA', '14:30:00', '2019-10-20'),
+(20, 'Muhammad Zaki', 'ZAK', '11:20:00', '2019-10-20'),
+(21, 'Dimas Aggung', 'DAG', '14:02:00', '2019-10-20'),
+(22, 'Nur Azizah Harun', 'CHA', '14:02:00', '2019-10-20'),
+(23, 'Ahmad Sambang Setya Tama', 'SAM', '14:00:00', '2019-10-20'),
+(24, 'Noufal Assidiq', 'NFL', '14:00:00', '2019-10-20'),
+(25, 'Bambang Utomo', 'BBG', '14:01:00', '2019-10-20'),
+(26, 'Ahmad Kurniawan', 'AKR', '14:00:00', '2019-10-20'),
+(27, 'Widya Febriyani', 'WDY', '14:00:00', '2019-10-20');
 
 -- --------------------------------------------------------
 
@@ -100,8 +87,7 @@ CREATE TABLE `request` (
   `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `kode` char(3) NOT NULL,
-  `shift` varchar(10) NOT NULL,
-  `jam` varchar(10) NOT NULL,
+  `jam` time(6) NOT NULL,
   `tanggal` varchar(20) NOT NULL,
   `gambar` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -110,8 +96,8 @@ CREATE TABLE `request` (
 -- Dumping data for table `request`
 --
 
-INSERT INTO `request` (`id`, `nama`, `kode`, `shift`, `jam`, `tanggal`, `gambar`) VALUES
-(1, 'Zaqlul', 'ZAQ', 'shift1', '15:20:30', '20/10/2019', '');
+INSERT INTO `request` (`id`, `nama`, `kode`, `jam`, `tanggal`, `gambar`) VALUES
+(22, 'Muhammad Arrasyid ', 'RSD', '01:58:57.000000', ' 05/04/19 ', '5ca6fcb8add5f.jpg');
 
 -- --------------------------------------------------------
 
@@ -122,41 +108,36 @@ INSERT INTO `request` (`id`, `nama`, `kode`, `shift`, `jam`, `tanggal`, `gambar`
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `kode` char(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(1, 'singgih', '$2y$10$6apOBX4vTuBC7InJ.HNZgu.uoMIHez4vOGSn11gdYIMmdjSpAod/6'),
-(2, 'zaqlul', '$2y$10$JRxA4qBuRV9kNY9aV9FvlejxbZjyb6KMZXNmT5TlcZcvSvWDmIEYC'),
-(7, 'icha', '$2y$10$uX/8xZm9kzT96M3enQWIHuGuACjp5o5nE3xt0wJ2W6CYh9uzgG/7.'),
-(8, 'bambang', '$2y$10$ObqQc6p.nslkCuDCqFBqOO9kyZOfsHB1OEBmju0h6dvdruJWmxd1e'),
-(9, 'sambang', '$2y$10$q/YvqEgWrOTdWSdI31WElewYoiTKMMC2lH5Y9M5wmAzuCIiUFv80q'),
-(10, 'samsa', '$2y$10$CKAL9CJxbBHKA5ix9VhEvOfv8a3FdO1EVGldpzTr7Cd1wlIErA7fm');
+INSERT INTO `user` (`id`, `username`, `password`, `nama`, `kode`) VALUES
+(16, 'rasyid', '$2y$10$fTWRX6nDf2MEQM7YT.Ejd.VtBhQS1fEZmrjf7j/ofkNNjms/e3UW6', 'Muhammad Arrasyid ', 'RSD'),
+(17, 'suryono', '$2y$10$g6j9tj1RlYz4TM2.QcWt6.3JVgMeGmKQTD5rHBaKipFhLwA5m61DS', 'Suryono Cahyadi', ''),
+(18, 'joko', '$2y$10$FNe/wy.wxdAflctw5WCOEe13n14.ZNKafOp3z8ONX46HyCGbfbnGu', 'Joko Siman', ''),
+(19, 'singgih', '$2y$10$UVgs.qK1r91YXJInjNqJuOdb7XyZiz7oA0y96B3TaMU.RzZiVSXoG', 'Singgih Aji Sasongko', ''),
+(20, 'jibon', '$2y$10$knAnWj3pWBpPniP.jpe1p.fpCjvJJelwxCpK96eZ6lw3uXo8feoiG', 'Singgih Aji S', 'SAS');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `assistant`
 --
 ALTER TABLE `assistant`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `mahasiswa1`
---
-ALTER TABLE `mahasiswa1`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -176,34 +157,28 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `assistant`
 --
 ALTER TABLE `assistant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `mahasiswa1`
---
-ALTER TABLE `mahasiswa1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `request`
 --
 ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
